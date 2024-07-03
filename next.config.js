4b6196b7d65615b687354604ec8ea736172dd006
NeilRow56 <neilrowland56@gmail.com>
2024-07-03 17:12:44 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,32 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'uploadthing.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'a0.muscache.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'files.edgestore.dev',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
